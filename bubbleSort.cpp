3d4001287e6c87adf2848d534eed158205e3cbb7
Faiqfarmz <faiqfarmz@gmail.com>
2025-03-13 11:56:17 +0700

    perubahan ketiga

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -46,3 +46,24 @@ void bubbleSortArray(){
     }  while (pass <= n - 1); // step5
 
 }
+
+
+void display() {
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++) {
+        cout << arr[j]; // menampilkan array
+        if (j < n - 1) {
+            cout << " --> ";
+        }
+    }
+    cout << endl;
+    cout << endl;
+
+    cout << "jumlah pass = " << n - 1 << endl; // menampilkan jumlah dar pass
+    cout << endl;
+    cout << endl;
+}
