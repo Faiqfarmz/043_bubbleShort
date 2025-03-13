b4bb044ca318996e4e54a6d86fd2cfe1daa268f7
Faiqfarmz <faiqfarmz@gmail.com>
2025-03-13 11:54:15 +0700

    perubahan pertama

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,3 +1,27 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int arr[20];    // Deklarasi variable global array a dengan ukuran 20
+int n;          // Deklarasi variable global n untuk menyimpan banyaknya elemen pada array
+
+void input () { // prosedur input
+    while (true) {
+        cout << "masukkan banyaknya elemen pada array : ";  // Output ke layar
+        cin >> n;   // input dari pengguna
+        if (n <= 20)    // jika n kurang dari atau sama dengan 20
+            break;
+        else {  // jika n lebih dari 20
+            cout << "\nArray dapat mempunyai maksimal 20 elemen/n" << endl;
+        }
+    }
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Masukkan Elemen Array" << endl;
+    cout << "=====================" << endl;
+
+    for (int i = 0; i <n; i++){
+        cout << "Data ke-" << (1+1) << ";" ;
+        cin >> arr [i];
+    }
+}  
